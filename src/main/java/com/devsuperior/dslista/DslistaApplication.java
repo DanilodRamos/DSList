1e7ab72d58b1e9cf6777c88bf7da8ef4ca82d168
danilo <danilodramos@yahoo.com.br>
2023-05-09 18:14:00 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslista/DslistaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslista/DslistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
